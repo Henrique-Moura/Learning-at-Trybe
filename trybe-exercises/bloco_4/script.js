67d71a24418de707ff5c5a2556dbb3aea748d09f
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-11-26 13:38:02 -0300

    Exercicio 1 - Faça cinco programas, um para cada operação aritmética básica

---
+++ trybe-exercises/bloco_4/script.js
@@ -0,0 +1,13 @@
+let a = 7;
+let b = 2;
+
+let soma = a + b;
+    console.log(soma);
+let subtracao = a -b;
+    console.log(subtracao);
+let multiplicacao = a * b;
+    console.log(multiplicacao);
+let divisao = a / b;
+    console.log(divisao);
+let modulo = a % b;
+    console.log(modulo);
