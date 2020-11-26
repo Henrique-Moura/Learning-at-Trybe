a3a9a4c4633f3bcba29317c98c83cb57d89405f0
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-11-26 13:57:17 -0300

    Exercicio 3 - Faça um programa que retorne o maior de três números

---
+++ trybe-exercises/bloco_4/Exercicio_3.js
@@ -0,0 +1,12 @@
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+let a = 2;
+let b = 7;
+let c = 12;
+
+if (a > b && a > c){
+    console.log(a)
+} else if (b > a && b > c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
