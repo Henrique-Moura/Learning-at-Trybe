6794980ab4bb78b3aedfa28e32a8d7aee623443c
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-11-27 13:16:38 -0300

    Exercicio 4 - retorne positive se esse valor for positivo, negative se for negativo e zero caso contrário

--- trybe-exercises/bloco_4/dia_2/exercicio_4.js
+++ trybe-exercises/bloco_4/dia_2/exercicio_4.js
@@ -1,1 +1,11 @@
 // Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+let numbers = [5, 9, 0, 19, 70, 8, 100, -1, 35, 27];
+    for(let index = 0; index < numbers.length; index+=1){
+        if(numbers[index] > 0) {
+            console.log('positive');
+        } else if (numbers[index] < 0) {
+            console.log('negative');
+        } else {
+            console.log('zero');
+        }
+    }
