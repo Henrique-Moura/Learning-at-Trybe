cffecbbb9bacd0209e768e51ce41fe14c2d647ad
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-11-27 12:14:06 -0300

    Exercicio 3 - calcule e imprima a média aritmética dos valores contidos no array

--- trybe-exercises/bloco_4/dia_2/exercicio_3.js
+++ trybe-exercises/bloco_4/dia_2/exercicio_3.js
@@ -1,2 +1,9 @@
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let divisao = numbers.length;
+    for(let index = 0; index < numbers.length; index += 1){
+        soma = soma + numbers[index];        
+    }
+    console.log(soma / divisao);
