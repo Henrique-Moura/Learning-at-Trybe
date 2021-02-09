f35713e08177003ada429d45c769c3e547802292
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-02-08 23:43:18 -0300

    4.3 concluido

--- trybe-exercises/bloco_4/dia_3/exercicio_1.js
+++ trybe-exercises/bloco_4/dia_3/exercicio_1.js
@@ -1,7 +1,6 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
-let numero = 7;
+let numero = 5;
 let asteriscos = "*";
-    for(let linha = 0; linha <= numero; linha += 1){
-        for (let coluna = 0; coluna <= numero; coluna += 1)
-        console.log(asteriscos);
-    } 
+for(let linha = 0; linha < numero; linha += 1){
+  console.log(asteriscos.repeat(numero));
+}
